4e474d1ee50f88ffd74254c7e56e327928df2b14
MiguelMSanchez <miguel.mchez@gmail.com>
2020-11-07 12:45:47 +0100

    version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArticulosComponent } from './shared/components/articulos/articulos.component';
+import { NoticiasComponent } from './shared/components/noticias/noticias.component';
+import { LoginComponent } from './shared/components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticulosComponent,
+    NoticiasComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
